5df73a02e1835a1ef171877bfe6afed06fb87fd2
captainboogey <anik35-700@diu.edu.bd>
2025-01-24 18:42:52 +0600

    marquee added

---
+++ src/Components/Sales.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const Sales = () => {
+    return (
+        <div className='flex gap-2'>
+            <button className='btn btn-primary'>Updates</button>
+            <Marquee>
+            <p className='ml-2'>Sold: Beautiful 3-bedroom villa in Palm Heights - $420,000!</p>
+            <p className='ml-2'>New Owner Alert: Cozy downtown condo sold for $325,000!</p>
+            <p className='ml-2'>Luxury waterfront home closed in Marina Bay - $1.2M!</p>
+            <p className='ml-2'>Historic 2-story cottage in cox sold at $285,000!</p>
+            </Marquee>
+        </div>
+    );
+};
+
+export default Sales;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar';
+import Sales from '../../Components/Sales';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Sales></Sales>
         </div>
     );
 };
